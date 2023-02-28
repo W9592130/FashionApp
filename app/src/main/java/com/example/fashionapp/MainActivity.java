ae3a1c81f718b4908753f67ae30bab8e92af9428
w9592130 <bhavanakancharla007@gmail.com>
2023-02-28 16:37:52 +0000

    Updated commit

--- app/src/main/java/com/example/fashionapp/MainActivity.java
+++ app/src/main/java/com/example/fashionapp/MainActivity.java
@@ -3,7 +3,7 @@ package com.example.fashionapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-
+// Bhavana
 public class MainActivity extends AppCompatActivity {
 
     @Override
